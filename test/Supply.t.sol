6e8838e6a514fb6bda0d84edaf50bf7bad1f7f00
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-12-20 09:19:30 -0500

    feat: supply failure tests done

--- test/Supply.t.sol
+++ test/Supply.t.sol
@@ -3,20 +3,115 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
+import { Errors } from "lib/aave-v3-core/contracts/protocol/libraries/helpers/Errors.sol";
+
+import { IAToken } from "lib/aave-v3-core/contracts/interfaces/IAToken.sol";
+
 import { SparklendTestBase } from "./SparklendTestBase.sol";
 
-contract SupplyTests is SparklendTestBase {
+contract SupplyConcreteFailureTests is SparklendTestBase {
 
     address supplier = makeAddr("supplier");
 
-    function test_supply() public {
-        uint256 amount = 1000 ether;
+    function test_supply_success_replaceThis() public {
+        vm.startPrank(supplier);
 
+        collateralAsset.mint(supplier, 1000 ether);
+        collateralAsset.approve(address(pool), 1000 ether);
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+    }
+
+    function test_supply_whenAmountZero() public {
+        vm.expectRevert(bytes(Errors.INVALID_AMOUNT));
+        pool.supply(address(collateralAsset), 0, supplier, 0);
+    }
+
+    function test_supply_whenNotActive() public {
+        vm.prank(admin);
+        poolConfigurator.setReserveActive(address(collateralAsset), false);
+
+        vm.expectRevert(bytes(Errors.RESERVE_INACTIVE));
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+    }
+
+    function test_supply_whenPaused() public {
+        vm.prank(admin);
+        poolConfigurator.setReservePause(address(collateralAsset), true);
+
+        vm.expectRevert(bytes(Errors.RESERVE_PAUSED));
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+    }
+
+    function test_supply_whenFrozen() public {
+        vm.prank(admin);
+        poolConfigurator.setReserveFreeze(address(collateralAsset), true);
+
+        vm.expectRevert(bytes(Errors.RESERVE_FROZEN));
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+    }
+
+    function test_supply_amountOverSupplyCapBoundary() public {
+        vm.prank(admin);
+        poolConfigurator.setSupplyCap(address(collateralAsset), 1000);
+
+        // Set up for success case
         collateralAsset.mint(supplier, 1000 ether);
 
         vm.startPrank(supplier);
+
         collateralAsset.approve(address(pool), 1000 ether);
-        pool.supply(address(collateralAsset), amount, supplier, 0);
+
+        // Boundary is 1 wei, not 1 ether even though supply cap is
+        // using units without decimals.
+        vm.expectRevert(bytes(Errors.SUPPLY_CAP_EXCEEDED));
+        pool.supply(address(collateralAsset), 1000 ether + 1, supplier, 0);
+
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+    }
+
+    function test_supply_insufficientApproveBoundary() public {
+        collateralAsset.mint(supplier, 1000 ether);
+
+        vm.startPrank(supplier);
+
+        collateralAsset.approve(address(pool), 1000 ether - 1);
+
+        vm.expectRevert(stdError.arithmeticError);
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+
+        collateralAsset.approve(address(pool), 1000 ether);
+
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+    }
+
+    function test_supply_insufficientBalanceBoundary() public {
+        vm.startPrank(supplier);
+
+        collateralAsset.approve(address(pool), 1000 ether);
+        collateralAsset.mint(supplier, 1000 ether - 1);
+
+        vm.expectRevert(stdError.arithmeticError);
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+
+        collateralAsset.mint(supplier, 1);
+
+        pool.supply(address(collateralAsset), 1000 ether, supplier, 0);
+    }
+
+    function test_supply_aTokenMintNotCalledByPool() public {
+        address aToken = pool.getReserveData(address(collateralAsset)).aTokenAddress;
+
+        vm.expectRevert(bytes(Errors.CALLER_MUST_BE_POOL));
+        IAToken(aToken).mint(address(this), address(this), 1000 ether, 1e18);
+    }
+
+    function test_supply_aTokenMintScaledInvalidAmount() public {
+        address aToken = pool.getReserveData(address(collateralAsset)).aTokenAddress;
+
+        vm.prank(address(pool));
+        vm.expectRevert(bytes(Errors.INVALID_MINT_AMOUNT));
+        IAToken(aToken).mint(address(this), address(this), 0, 1e18);
     }
 
 }
+
