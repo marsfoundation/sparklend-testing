467cbc40d48dbc637d16a055c5a2a6d34c96a58b
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-02-05 08:58:07 -0600

    feat: add more failure testing

--- test/Withdraw.t.sol
+++ test/Withdraw.t.sol
@@ -31,4 +31,65 @@ contract WithdrawFailureTests is WithdrawTestBase {
         pool.withdraw(address(collateralAsset), 0, user);
     }
 
+    function test_withdraw_amountGtBalanceBoundary() public {
+        vm.startPrank(user);
+        vm.expectRevert(bytes(Errors.NOT_ENOUGH_AVAILABLE_USER_BALANCE));
+        pool.withdraw(address(collateralAsset), 1000 ether + 1, user);
+
+        pool.withdraw(address(collateralAsset), 1000 ether, user);
+    }
+
+    // TODO: Unreachable code - setReserveActive reverts with RESERVE_LIQUIDITY_NOT_ZERO, can't withdraw without liquidity
+    // function test_withdraw_whenNotActive() public {
+    //     vm.prank(admin);
+    //     poolConfigurator.setReserveActive(address(collateralAsset), false);
+
+    //     vm.expectRevert(bytes(Errors.RESERVE_INACTIVE));
+    //     pool.withdraw(address(collateralAsset), 1000 ether, user);
+    // }
+
+    function test_withdraw_whenPaused() public {
+        vm.prank(admin);
+        poolConfigurator.setReservePause(address(collateralAsset), true);
+
+        vm.prank(user);
+        vm.expectRevert(bytes(Errors.RESERVE_PAUSED));
+        pool.withdraw(address(collateralAsset), 1000 ether, user);
+    }
+
+    // TODO: Extra test, determine convention for this
+    function test_withdraw_success_whenFrozen() public {
+        vm.prank(admin);
+        poolConfigurator.setReserveFreeze(address(collateralAsset), true);
+
+        vm.prank(user);
+        pool.withdraw(address(collateralAsset), 1000 ether, user);
+    }
+
+    function test_withdraw_healthFactorBelowThresholdBoundary() public {
+        _initCollateral({
+            asset:                address(collateralAsset),
+            ltv:                  5000,
+            liquidationThreshold: 5000,
+            liquidationBonus:     100_01
+        });
+
+        vm.prank(user);
+        pool.setUserUseReserveAsCollateral(address(collateralAsset), true);
+
+        vm.prank(admin);
+        poolConfigurator.setReserveBorrowing(address(borrowAsset), true);
+
+        _supply(makeAddr("supplier"), address(borrowAsset), 250 ether);
+        _borrow(user, address(borrowAsset), 250 ether);
+
+        // NOTE: 1e10 used for boundary since HF calculations are done in 1e8 precision
+        vm.startPrank(user);
+
+        vm.expectRevert(bytes(Errors.HEALTH_FACTOR_LOWER_THAN_LIQUIDATION_THRESHOLD));
+        pool.withdraw(address(collateralAsset), 500 ether + 1e10 + 1, user);
+
+        pool.withdraw(address(collateralAsset), 500 ether + 1e10, user);
+    }
+
 }
