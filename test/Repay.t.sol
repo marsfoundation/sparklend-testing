65c98241912882607415db99a3803002903fa7c5
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-02-08 09:07:18 -0600

    feat: failure mode tests complete

--- test/Repay.t.sol
+++ test/Repay.t.sol
@@ -44,6 +44,58 @@ contract RepayFailureTests is RepayTestBase {
 
     function test_repay_whenAmountZero() public {
         vm.expectRevert(bytes(Errors.INVALID_AMOUNT));
-        pool.repay(address(borrowAsset), 0, 0, borrower);
+        pool.repay(address(borrowAsset), 0, 2, borrower);
     }
+
+    function test_repay_whenAmountIsUint256MaxAndUserNotOwner() public {
+        vm.expectRevert(bytes(Errors.NO_EXPLICIT_AMOUNT_TO_REPAY_ON_BEHALF));
+        pool.repay(address(borrowAsset), type(uint256).max, 2, borrower);
+    }
+
+    // TODO: Believe this code is unreachable because can't be set to inactive when there is active
+    //       borrows.
+    // function test_repay_whenNotActive() public {
+    //     vm.prank(admin);
+    //     poolConfigurator.setReserveActive(address(borrowAsset), false);
+
+    //     vm.expectRevert(bytes(Errors.RESERVE_INACTIVE));
+    //     pool.repay(address(borrowAsset), 500 ether, 2, borrower);
+    // }
+
+    function test_repay_whenPaused() public {
+        vm.prank(admin);
+        poolConfigurator.setReservePause(address(borrowAsset), true);
+
+        vm.startPrank(borrower);
+        borrowAsset.approve(address(pool), 500 ether);
+
+        vm.expectRevert(bytes(Errors.RESERVE_PAUSED));
+        pool.repay(address(borrowAsset), 500 ether, 2, borrower);
+    }
+
+    // TODO: Determine convention for this
+    function test_repay_success_whenFrozen() public {
+        vm.prank(admin);
+        poolConfigurator.setReserveFreeze(address(borrowAsset), true);
+
+        vm.startPrank(borrower);
+        borrowAsset.approve(address(pool), 500 ether);
+        pool.repay(address(borrowAsset), 500 ether, 2, borrower);
+    }
+
+    function test_repay_noDebt() public {
+        vm.expectRevert(bytes(Errors.NO_DEBT_OF_SELECTED_TYPE));
+        pool.repay(address(borrowAsset), 500 ether, 2, lender);
+    }
+
+    function test_repay_notEnoughApprovalOfUnderlyingBoundary() public {
+        vm.startPrank(borrower);
+        borrowAsset.approve(address(pool), 500 ether - 1);
+        vm.expectRevert(stdError.arithmeticError);
+        pool.repay(address(borrowAsset), 500 ether, 2, borrower);
+
+        borrowAsset.approve(address(pool), 500 ether);
+        pool.repay(address(borrowAsset), 500 ether, 2, borrower);
+    }
+
 }
