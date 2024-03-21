7af27bda737f1cd673c70eb8b1a846d4624a59e4
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-03-21 08:40:55 -0600

    feat: set up base file

---
+++ test/integration/IntegrationTestBase.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import "sparklend-address-registry/Ethereum.sol";
+
+contract IntegrationTestBase is Test {
+
+}
+
