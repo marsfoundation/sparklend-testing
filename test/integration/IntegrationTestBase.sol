b2317d64663088c9be6a7264aa03ecf3da96ec64
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-03-21 08:59:00 -0600

    feat: move user actions into dedicated file

--- test/integration/IntegrationTestBase.sol
+++ test/integration/IntegrationTestBase.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 
+import { IPool } from "aave-v3-core/contracts/interfaces/IPool.sol";
+
 import "sparklend-address-registry/Ethereum.sol";
 
 contract IntegrationTestBase is Test {
+
+    IPool constant pool = IPool(Ethereum.POOL);
 
     function setUp() public {
 
---
+++ test/integration/StableBorrow.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IntegrationTestBase } from "./IntegrationTestBase.sol";
+
+contract StableBorrowTests is IntegrationTestBase {
+
+    address borrower = makeAddr("borrower");
+
+    function testStableBorrow() public {
+        // Perform necessary setup for the test
+
+        // Attempt to stable borrow from Aave
+        // ...
+
+        // Assert that the stable borrow fails
+        // ...
+    }
+}
