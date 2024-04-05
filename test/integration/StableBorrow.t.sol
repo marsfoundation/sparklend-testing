ed50b66eea3a9c87e03a2677ef3f42a46d9a2807
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-04-05 09:25:49 -0400

    feat: working rebalance test

--- test/integration/StableBorrow.t.sol
+++ test/integration/StableBorrow.t.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IPoolAddressesProvider } from "sparklend-v1-core/contracts/interfaces/IPoolAddressesProvider.sol";
+import "forge-std/Test.sol";
 
-import { ReserveConfiguration } from "sparklend-v1-core/contracts/protocol/libraries/configuration/ReserveConfiguration.sol";
+import { IERC20 } from "erc20-helpers/interfaces/IERC20.sol";
 
-import { DataTypes } from "sparklend-v1-core/contracts/protocol/libraries/types/DataTypes.sol";
-import { Errors }    from "sparklend-v1-core/contracts/protocol/libraries/helpers/Errors.sol";
+import { Ethereum } from "sparklend-address-registry/Ethereum.sol";
 
 import { VariableBorrowInterestRateStrategy } from "sparklend-advanced/VariableBorrowInterestRateStrategy.sol";
 
-import { Ethereum } from "sparklend-address-registry/Ethereum.sol";
+import { IPoolAddressesProvider } from "sparklend-v1-core/contracts/interfaces/IPoolAddressesProvider.sol";
+
+import { ReserveConfiguration } from "sparklend-v1-core/contracts/protocol/libraries/configuration/ReserveConfiguration.sol";
+import { DataTypes }            from "sparklend-v1-core/contracts/protocol/libraries/types/DataTypes.sol";
+import { Errors }               from "sparklend-v1-core/contracts/protocol/libraries/helpers/Errors.sol";
 
 import { IntegrationTestBase } from "./IntegrationTestBase.sol";
 
@@ -111,34 +114,68 @@ contract StableBorrowTests is IntegrationTestBase {
         }
     }
 
-    function test_rebalanceStableBorrowRateAfterIrmChange() public {
-        uint256 currentLiquidityRate = pool.getReserveData(Ethereum.WETH).currentLiquidityRate;
+    function test_rebalanceStableBorrowRateAfterIrmChangeBoundary() public {
+        // TODO: Remove
+        _etchLibrary(Ethereum.BORROW_LOGIC);
 
-        assertEq(currentLiquidityRate, 0.017182675769211267556222548e27);
+        address stableDebtToken = deployCode("StableDebtToken.sol:StableDebtToken", bytes(abi.encode(address(pool))));
+        // address deployedBorrowLogic = 0x4662C88C542F0954F8CccCDE4542eEc32d7E7e9a;
+        vm.etch(Ethereum.DAI_STABLE_DEBT_TOKEN, stableDebtToken.code);
 
+        // Set the supply cap to be higher so the borrower can post enough ETH to borrow all the DAI
+        vm.prank(Ethereum.SPARK_PROXY);
+        poolConfigurator.setSupplyCap(Ethereum.WETH, 10_000_000);
+
+        _supplyAndUseAsCollateral(borrower, Ethereum.WETH, 1_000_000 ether);
+
+        // Borrow all the DAI so that liquidityRate == borrowRate for easier test configuration
+        _borrow(borrower, Ethereum.DAI, IERC20(Ethereum.DAI).balanceOf(Ethereum.DAI_ATOKEN));
+
+        uint256 currentLiquidityRate = pool.getReserveData(Ethereum.DAI).currentLiquidityRate;
+
+        assertEq(currentLiquidityRate, 0.148420005467532821842464000e27);
+
+        // Rebalance fails under normal conditions
+        vm.prank(borrower);
+        vm.expectRevert(bytes(Errors.INTEREST_RATE_REBALANCE_CONDITIONS_NOT_MET));
+        pool.rebalanceStableBorrowRate(Ethereum.DAI, borrower);
+
+        // Update the strategy to boundary condition of 90% of liquidity rate check
         address strategy = address(new VariableBorrowInterestRateStrategy({
             provider:               IPoolAddressesProvider(Ethereum.POOL_ADDRESSES_PROVIDER),
             optimalUsageRatio:      1e27,
-            baseVariableBorrowRate: currentLiquidityRate * 80 / 100,
+            baseVariableBorrowRate: currentLiquidityRate * 100 / 90 - 1,
             variableRateSlope1:     0,
             variableRateSlope2:     0
         }));
 
         vm.prank(Ethereum.SPARK_PROXY);
-        poolConfigurator.setReserveInterestRateStrategyAddress(Ethereum.WETH, strategy);
+        poolConfigurator.setReserveInterestRateStrategyAddress(Ethereum.DAI, strategy);
 
-        _supplyAndUseAsCollateral(borrower, Ethereum.WETH, 1000 ether);
-
-        _borrow(borrower, Ethereum.DAI, 100);
-
-        _etchLibrary(Ethereum.BORROW_LOGIC);
-
-        // address borrowLogic = deployCode("BorrowLogic.sol:BorrowLogic", bytes(""));
-        // address deployedBorrowLogic = 0x4662C88C542F0954F8CccCDE4542eEc32d7E7e9a;
-        // vm.etch(deployedBorrowLogic, borrowLogic.code);
-
+        // Same error
         vm.prank(borrower);
-        // vm.expectRevert(bytes(Errors.INTEREST_RATE_REBALANCE_CONDITIONS_NOT_MET));
+        vm.expectRevert(bytes(Errors.INTEREST_RATE_REBALANCE_CONDITIONS_NOT_MET));
+        pool.rebalanceStableBorrowRate(Ethereum.DAI, borrower);
+
+        // Update the strategy to pass the 90% check
+        strategy = address(new VariableBorrowInterestRateStrategy({
+            provider:               IPoolAddressesProvider(Ethereum.POOL_ADDRESSES_PROVIDER),
+            optimalUsageRatio:      1e27,
+            baseVariableBorrowRate: currentLiquidityRate * 100 / 90,
+            variableRateSlope1:     0,
+            variableRateSlope2:     0
+        }));
+
+        vm.prank(Ethereum.SPARK_PROXY);
+        poolConfigurator.setReserveInterestRateStrategyAddress(Ethereum.DAI, strategy);
+
+        // Get a different error, an EVM revert on line 146 of StableDebtToken.sol
+        // Revert occurs in `rayDiv((currentBalance + amount).wadToRay())` portion of calculation
+        // in the `mint` function because `currentBalance` and `amount` are both zero, resulting in
+        // a division by zero. Since a user can not mint stable debt through the SparkLend protocol,
+        // This function cannot ever be successfully called.
+        vm.prank(borrower);
+        vm.expectRevert(bytes(""));  // EvmError: Revert
         pool.rebalanceStableBorrowRate(Ethereum.DAI, borrower);
     }
 
