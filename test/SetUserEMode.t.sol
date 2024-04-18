58ef519f73c98b387befcd4da66cb45d628392ef
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-04-18 14:08:22 -0400

    feat: add remaining success tests

--- test/SetUserEMode.t.sol
+++ test/SetUserEMode.t.sol
@@ -49,11 +49,11 @@ contract SetUserEModeTestBase is SparkLendTestBase {
         poolConfigurator.setAssetEModeCategory(address(borrowAsset),     2);
 
         vm.stopPrank();
-
-        _supplyAndUseAsCollateral(borrower, address(collateralAsset), 1000 ether);
     }
 
 }
+
+// TODO: Use `proveNoOp` wherever applicable
 
 contract SetUserEModeFailureTests is SetUserEModeTestBase {
 
@@ -66,6 +66,8 @@ contract SetUserEModeFailureTests is SetUserEModeTestBase {
         vm.prank(borrower);
         pool.setUserEMode(1);
 
+        _supplyAndUseAsCollateral(borrower, address(collateralAsset), 1000 ether);
+
         _borrow(borrower, address(collateralAsset), 100 ether);
 
         vm.prank(borrower);
@@ -76,6 +78,8 @@ contract SetUserEModeFailureTests is SetUserEModeTestBase {
     function test_setUserEMode_userExitsEModeAndGoesBelowHFBoundary() public {
         vm.prank(borrower);
         pool.setUserEMode(1);
+
+        _supplyAndUseAsCollateral(borrower, address(collateralAsset), 1000 ether);
 
         _borrow(borrower, address(collateralAsset), 500 ether);  // Right at 50% LT
 
@@ -99,6 +103,204 @@ contract SetUserEModeFailureTests is SetUserEModeTestBase {
         vm.prank(borrower);
         vm.expectRevert(bytes(Errors.HEALTH_FACTOR_LOWER_THAN_LIQUIDATION_THRESHOLD));
         pool.setUserEMode(0);
+    }
+
+}
+
+contract SetUserEModeSuccessTests is SetUserEModeTestBase {
+
+    modifier whenUserHasNotSuppliedAnyAssets { _; }
+
+    modifier whenUserHasSuppliedAnAsset {
+        _supply(borrower, address(collateralAsset), 1000 ether);
+        _;
+    }
+
+    modifier whenUserDoesNotHaveActiveEMode { _; }
+
+    modifier whenUserHasActiveEMode {
+        vm.prank(borrower);
+        pool.setUserEMode(1);
+
+        assertEq(pool.getUserEMode(borrower), 1);
+
+        _;
+    }
+
+    modifier whenUserHasAnActiveBorrow {
+        vm.prank(borrower);
+        pool.setUserUseReserveAsCollateral(address(collateralAsset), true);
+
+        // NOTE: Borrow collateralAsset because its in same e-mode
+        _borrow(borrower, address(collateralAsset), 100 ether);
+        _;
+    }
+
+    modifier whenUserDoesNotHaveAnActiveBorrow { _; }
+
+    function test_setUserEMode_01() 
+        public 
+        whenUserHasNotSuppliedAnyAssets 
+        whenUserDoesNotHaveActiveEMode
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(0);
+
+        assertEq(pool.getUserEMode(borrower), 0);
+    }
+
+    function test_setUserEMode_02() 
+        public 
+        whenUserHasNotSuppliedAnyAssets 
+        whenUserDoesNotHaveActiveEMode
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(1);
+
+        assertEq(pool.getUserEMode(borrower), 1);
+    }
+
+    function test_setUserEMode_03() 
+        public 
+        whenUserHasNotSuppliedAnyAssets 
+        whenUserHasActiveEMode
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(0);
+
+        assertEq(pool.getUserEMode(borrower), 0);
+    }
+
+    function test_setUserEMode_04() 
+        public 
+        whenUserHasNotSuppliedAnyAssets 
+        whenUserHasActiveEMode
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(1);
+
+        assertEq(pool.getUserEMode(borrower), 1);
+    }
+
+    function test_setUserEMode_05() 
+        public 
+        whenUserHasNotSuppliedAnyAssets 
+        whenUserHasActiveEMode
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(2);
+
+        assertEq(pool.getUserEMode(borrower), 2);
+    }
+
+    function test_setUserEMode_06() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserDoesNotHaveActiveEMode
+        whenUserDoesNotHaveAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(0);
+
+        assertEq(pool.getUserEMode(borrower), 0);
+    }
+
+    function test_setUserEMode_07() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserDoesNotHaveActiveEMode
+        whenUserDoesNotHaveAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(1);
+
+        assertEq(pool.getUserEMode(borrower), 1);
+    }
+
+    function test_setUserEMode_08() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserDoesNotHaveActiveEMode
+        whenUserHasAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(0);
+
+        assertEq(pool.getUserEMode(borrower), 0);
+    }
+
+    function test_setUserEMode_09() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserDoesNotHaveActiveEMode
+        whenUserHasAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(1);
+
+        assertEq(pool.getUserEMode(borrower), 1);
+    }
+
+    function test_setUserEMode_10() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserHasActiveEMode
+        whenUserDoesNotHaveAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(0);
+
+        assertEq(pool.getUserEMode(borrower), 0);
+    }
+
+    function test_setUserEMode_11() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserHasActiveEMode
+        whenUserDoesNotHaveAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(1);
+
+        assertEq(pool.getUserEMode(borrower), 1);
+    }
+
+    // Why is this possible? A user can change to an emode that doesn't match the asset
+    // they have supplied. It only checks their active borrows. Can this be abused?
+    function test_setUserEMode_INVESTIGATE() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserHasActiveEMode
+        whenUserDoesNotHaveAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(2);
+
+        assertEq(pool.getUserEMode(borrower), 2);
+    }
+
+    function test_setUserEMode_12() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserHasActiveEMode
+        whenUserHasAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(0);
+
+        assertEq(pool.getUserEMode(borrower), 0);
+    }
+
+    function test_setUserEMode_13() 
+        public 
+        whenUserHasSuppliedAnAsset
+        whenUserHasActiveEMode
+        whenUserHasAnActiveBorrow
+    {
+        vm.prank(borrower);
+        pool.setUserEMode(1);
+
+        assertEq(pool.getUserEMode(borrower), 1);
     }
 
 }
